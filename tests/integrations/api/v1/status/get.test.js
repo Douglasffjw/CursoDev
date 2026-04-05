935347921866bc56e1ab4ffc599af9c1eee2706f
Douglasffjw <douglasffjw@gmail.com>
2026-04-05 16:17:38 -0300

    usando docker

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,4 +1,7 @@
-test("GET to /api/v1/status should return  200 ", async () => {
+test("GET to /api/v1/status should return  200 ", async () => { 
+  // O async serve para que possamos usar o await dentro do teste,
+  // e o await é usado para esperar a resposta da requisição fetch antes
+  // de continuar com as asserções.
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
