8baf37ac4ad23874ec7cc8653ed73b259fa28fde
Douglasffjw <douglasffjw@gmail.com>
2026-04-07 15:35:27 -0300

    adiciona `database.js` and `.env` files

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,7 +1,6 @@
-test("GET to /api/v1/status should return  200 ", async () => { 
-  // O async serve para que possamos usar o await dentro do teste,
-  // e o await é usado para esperar a resposta da requisição fetch antes
-  // de continuar com as asserções.
+
+
+test("GET to /api/v1/status should return  200 ", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-});
+}) ;
